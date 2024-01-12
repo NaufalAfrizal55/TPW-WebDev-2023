ec8f177611f5e86157789a1bab03543116ccf8d2
NaufalAfrizal55 <naufalafrizal02@gmail.com>
2024-01-12 14:13:09 +0700

    update for deploy

--- backend/api/controllers/googleAuth.js
+++ backend/api/controllers/googleAuth.js
@@ -6,7 +6,7 @@ const generateToken = require('../utils/createToken')
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-    'https://hungry-slug-moccasins.cyclic.app/api/auth/google/callback'
+    'https://tpw-web-dev-2023-backend.vercel.app/api/auth/google/callback'
 )
 
 const scopes = [
